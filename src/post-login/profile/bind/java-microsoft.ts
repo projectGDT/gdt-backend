ab645b00bc5a4f9ea50df5c636750b92dd06f05c
Wesley F. Young <willamwei@outlook.com>
2024-02-01 23:29:44 +0800

    repair java profile binding api

--- src/post-login/profile/bind/java-microsoft.ts
+++ src/post-login/profile/bind/java-microsoft.ts
@@ -1,56 +1,97 @@
 import {Express} from "express";
 import {PrismaClient} from "@prisma/client";
-import {validator} from "@exodus/schemasafe";
 import {Request} from "express-jwt";
+import {Authflow, Titles} from "prismarine-auth";
+import {clearTimeout} from "node:timers";
 
-const profileBindMicrosoftValidator = validator({
-    type: "object",
-    properties: {
-        accessToken: {type: "string"}
-    }
-})
+class EmptyCache {
+    async getCached () {}
+    async setCached (value: any) {}
+    async setCachedPartial (value: any) {}
+}
 
-module.exports = (app: Express, prisma: PrismaClient) => app.post("/post-login/profile/bind/java-microsoft", async (req: Request, res) => {
-    if (!profileBindMicrosoftValidator(req.body)) {
-        res.status(400).end()
-        return
-    }
+function emptyCacheFactory(object: any) {
+    return new EmptyCache()
+}
 
-    // ref: Microsoft Authentication Scheme - https://wiki.vg/Microsoft_Authentication_Scheme
+const sessionLifetime = 300 * 1000
 
-    const {accessToken} = req.body
-
-    await fetch("https://api.minecraftservices.com/minecraft/profile", {
-        headers: {
-            "Authorization": `Bearer ${accessToken}`,
-            "Accept": "application/json"
-        }
+module.exports = (app: Express, prisma: PrismaClient) => app.get("/post-login/profile/bind/java-microsoft", async (req: Request, res) => {
+    res.writeHead(200, {
+        "Connection": "keep-alive",
+        "Cache-Control": "no-cache",
+        "Content-Type": "text/event-stream" // this indicates a Server-Side Event connection
     })
-        .then(response => response.json())
-        .then(async json => {
-            if (!(json.id && json.name)) {
-                return Promise.reject()
+
+    let codeWritten = false
+
+    const authFlow = new Authflow(
+        undefined,
+        emptyCacheFactory, // we do not need to store any personal information
+        {
+            flow: "sisu",
+            authTitle: Titles.MinecraftJava,
+            deviceType: "Win32" // for MinecraftJava
+        },
+        codeResponse => {
+            if (!codeWritten) {
+                res.write(`data: ${JSON.stringify({
+                    userCode: codeResponse.user_code,
+                    verificationUri: codeResponse.verification_uri
+                })}\n\n`) // \n\n indicates that a data chunk ends
+                codeWritten = true
             }
+        }
+        // retried 2 times (done 3 times in total)
+        // but we do not want the code to be written twice
+    )
 
-            const idStr = json.id as string
-            // the returned UUID is without "-".
+    let timeoutRef: NodeJS.Timeout
 
-            const uuid =
-                `${idStr.slice(0, 8)}-${idStr.slice(8, 12)}-${idStr.slice(12, 16)}-${idStr.slice(16, 20)}-${idStr.slice(20, /* towards the end */)}`
-            const playerName = json.name
+    const result = await Promise.race([
+        authFlow.getMinecraftJavaToken({
+            fetchProfile: true
+        }).then(tokenResponse => {
+            const idStr = tokenResponse.profile.id
+            return {
+                success: true,
+                uuid: `${idStr.slice(0, 8)}-${idStr.slice(8, 12)}-${idStr.slice(12, 16)}-${idStr.slice(16, 20)}-${idStr.slice(20, /* towards the end */)}`,
+                playerName: tokenResponse.profile.name
+            }
+        }).catch(_err => {
+            return {
+                success: false,
+                reason: "internal-error"
+            }
+        }),
+        new Promise<{ success: boolean, reason: string }>((resolve, _reject) => {
+            timeoutRef = setTimeout(() => {
+                resolve({
+                    success: false,
+                    reason: "timeout"
+                })
+            }, sessionLifetime)
+        })
+    ])
+    // the original lifetime, 900 seconds, is too long
 
-            await prisma.profile.create({
-                data: {
-                    uniqueIdProvider: -1, // Java Microsoft
-                    uniqueId: uuid,
-                    playerId: req.auth.id,
-                    cachedPlayerName: playerName
-                }
-            })
+    if (result.success) {
+        clearTimeout(timeoutRef)
+        const {uuid, playerName} = <{
+            success: boolean
+            uuid: string,
+            playerName: string
+        }>result
+        await prisma.profile.create({
+            data: {
+                uniqueIdProvider: -1, // Java Microsoft
+                uniqueId: uuid,
+                playerId: req.auth.id,
+                cachedPlayerName: playerName
+            }
+        })
+    }
 
-            res.json({
-                uuid: uuid,
-                playerName: playerName
-            })
-        }).catch(_err => res.status(502).end())
+    res.write(`data: ${JSON.stringify(result)}`)
+    res.end()
 })
