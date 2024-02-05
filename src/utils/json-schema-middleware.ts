59d11aaa4b92dbe79e355baf2711734456fb68d1
Wesley F. Young <willamwei@outlook.com>
2024-02-05 22:26:04 +0800

    add json schema middleware

---
+++ src/utils/json-schema-middleware.ts
@@ -0,0 +1,15 @@
+import {Schema, validator} from "@exodus/schemasafe";
+import {Request as JWTRequest} from "express-jwt";
+import {NextFunction, Response} from "express";
+
+export function jsonValidate(
+    schema: Schema,
+    errorAction = (res: Response) => res.status(400).end()
+): (req: JWTRequest, res: Response, next: NextFunction) => void {
+    const jsonValidator = validator(schema)
+    return (req, res, next) => {
+        if (!jsonValidator(req.body))
+            errorAction(res)
+        else next()
+    }
+}
