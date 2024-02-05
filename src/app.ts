67aa5546c13d928ddfb1cfb07b195feb883b8ee5
Wesley F. Young <willamwei@outlook.com>
2024-02-06 00:46:29 +0800

    add indexes of new modules to app.ts

--- src/app.ts
+++ src/app.ts
@@ -30,8 +30,13 @@ require("./http/login")(app, prisma)
 
 require("./utils/auth-middleware")(app)
 
+require("./http/post-login/profile/fetch")(app, prisma)
 require("./http/post-login/profile/bind/java-microsoft")(app, prisma)
 require("./http/post-login/profile/bind/xbox")(app, prisma)
+
+require("./http/post-login/me/servers")(app, prisma)
+require("./http/post-login/me/discover/list")(app, prisma)
+require("./http/post-login/me/discover/query")(app, prisma)
 
 app.listen(port, () => {
     console.log(`Start Listening on port ${port}`)
