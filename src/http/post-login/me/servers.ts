79307af8936ce5172da1b1926ddd025ae3a3ac7c
Wesley F. Young <willamwei@outlook.com>
2024-02-06 00:13:11 +0800

    add query param "includeRemote" to servers.ts

--- src/http/post-login/me/servers.ts
+++ src/http/post-login/me/servers.ts
@@ -3,6 +3,8 @@ import {PrismaClient} from "@prisma/client";
 import {Request} from "express-jwt";
 
 module.exports = (app: Express, prisma: PrismaClient) => app.get("/post-login/me/servers", async (req: Request, res) => {
+    const includeRemote = Boolean(req.query.includeRemote)
+
     prisma.playerInServer.findMany({
         where: {
             playerId: req.auth.id
@@ -10,28 +12,31 @@ module.exports = (app: Express, prisma: PrismaClient) => app.get("/post-login/me
         include: {
             server: {
                 include: {
-                    javaRemote: true,
-                    bedrockRemote: true
+                    javaRemote: includeRemote,
+                    bedrockRemote: includeRemote
                 }
             }
         }
     }).then(result => result.map(
-        ({server, isOperator}) => ({
-            server: {
-                id: server.id,
-                name: server.name,
-                logoLink: server.logoLink,
-                javaRemote: {
-                    address: server.javaRemote.address,
-                    port: server.javaRemote.port
-                },
-                bedrockRemote: {
-                    address: server.bedrockRemote.address,
-                    port: server.bedrockRemote.port
-                },
-                applyingPolicy: server.applyingPolicy
-            },
-            isOperator: isOperator
+        ({server}) => ({
+            id: server.id,
+            name: server.name,
+            logoLink: server.logoLink,
+            ...includeRemote ? {
+                ...server.javaRemote ? {
+                    javaRemote: {
+                        address: server.javaRemote.address,
+                        port: server.javaRemote.port
+                    }
+                } : {},
+                ...server.bedrockRemote ? {
+                    bedrockRemote: {
+                        address: server.bedrockRemote.address,
+                        port: server.bedrockRemote.port
+                    }
+                } : {}
+            } : {},
+            applyingPolicy: server.applyingPolicy
         })
-    )).then(result => res.json(result))
+    )).then(res.json)
 })
