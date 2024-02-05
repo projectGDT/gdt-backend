accdf4a6efca202e812c76f6dcc222b499d5a128
Wesley F. Young <willamwei@outlook.com>
2024-02-06 00:12:42 +0800

    implement batch query

---
+++ src/http/post-login/me/discover/query.ts
@@ -0,0 +1,53 @@
+import {Express} from "express";
+import {PrismaClient} from "@prisma/client";
+import {Request} from "express-jwt";
+import {jsonValidate} from "../../../../utils/json-schema-middleware";
+
+module.exports = (app: Express, prisma: PrismaClient) => app.post(
+    "/post-login/me/discover/query",
+    jsonValidate({
+        type: "array",
+        items: {
+            type: "number"
+        },
+        uniqueItems: true,
+        maxItems: 10
+    }),
+    async (req: Request, res) => {
+        const includeRemote = Boolean(req.query.includeRemote)
+        const body = <number[]>req.body
+
+        prisma.server.findMany({
+            where: {
+                id: {
+                    in: body
+                }
+            },
+            include: {
+                javaRemote: true,
+                bedrockRemote: true
+            }
+        }).then(result => result.map(server => ({
+            id: server.id,
+            name: server.name,
+            logoLink: server.logoLink,
+            ...includeRemote ? {
+                ...server.javaRemote ? {
+                    javaRemote: {
+                        address: server.javaRemote.address,
+                        port: server.javaRemote.port
+                    }
+                } : {},
+                ...server.bedrockRemote ? {
+                    bedrockRemote: {
+                        address: server.bedrockRemote.address,
+                        port: server.bedrockRemote.port
+                    }
+                } : {}
+            } : {},
+            applyingPolicy: server.applyingPolicy
+        }))).then(result => result.sort(
+            (a, b) => body.indexOf(a.id) - body.indexOf(b.id) )
+        ).then(res.json)
+    }
+)
