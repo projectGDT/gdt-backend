cbf46e653aa24a73e5f1dac447db92b1a85fa250
Wesley F. Young <willamwei@outlook.com>
2024-02-05 22:33:56 +0800

    rewrite implementations using new middleware

--- src/http/login.ts
+++ src/http/login.ts
@@ -1,51 +1,48 @@
 import {Express} from "express"
 import {matches} from "../utils/digest"
-import {validator} from "@exodus/schemasafe"
 import {PrismaClient} from "@prisma/client"
 import jwt = require("jsonwebtoken")
 import {jwtSecret} from "../app"
-
-const loginValidator = validator({
-    type: "object",
-    properties: {
-        username: {type: "string", pattern: "^.{3,}$"},
-        password: {type: "string", pattern: "^.+$"},
-        "cf-turnstile-response": {type: "string"}
-    },
-    required: ["username", "password", "cf-turnstile-response"],
-    additionalProperties: false
-})
+import {jsonValidate} from "../utils/json-schema-middleware";
 const qidRegex = /^[1-9][0-9]{4,9}$/
 
-module.exports = (app: Express, prisma: PrismaClient) => app.post("/login", async (req, res) => {
-    if (!loginValidator(req.body)) {
-        res.status(400).end()
-        return
-    }
-
-    const {username, password, ["cf-turnstile-response"]: cfTurnstileResponse} = req.body
-    if (!(await require("../utils/captcha-verify")(cfTurnstileResponse))) {
-        res.status(400).end()
-        return
-    }
-
-    prisma.player.findUniqueOrThrow({
-        where: username.match(qidRegex) ?   // check if "username" is a valid qid
-            {qid: parseInt(username)} :     // use qid
-            {username: username}            // use username
-    }).then(async player => {
-        if (!matches(password, player.pwDigested))
-            return Promise.reject()
-        const payload = {
-            id: player.id,
-            isSiteAdmin: player.isSiteAdmin,
-            authorizedServers: await prisma.playerInServer.findMany({
-                where: {
-                    playerId: player.id,
-                    isOperator: true
-                }
-            }).then(result => result.map(entry => entry.serverId))
+module.exports = (app: Express, prisma: PrismaClient) => app.post(
+    "/login",
+    jsonValidate({
+        type: "object",
+        properties: {
+            username: {type: "string", pattern: "^.{3,}$"},
+            password: {type: "string", pattern: "^.+$"},
+            "cf-turnstile-response": {type: "string"}
+        },
+        required: ["username", "password", "cf-turnstile-response"],
+        additionalProperties: false
+    }),
+    async (req, res) => {
+        const {username, password, ["cf-turnstile-response"]: cfTurnstileResponse} = req.body
+        if (!(await require("../utils/captcha-verify")(cfTurnstileResponse))) {
+            res.status(400).end()
+            return
         }
-        res.json({...payload, jwt: jwt.sign(payload, jwtSecret, {expiresIn: "12h"})})
-    }).catch(_error => res.status(400).end())
-})
+
+        prisma.player.findUniqueOrThrow({
+            where: username.match(qidRegex) ?   // check if "username" is a valid qid
+                {qid: parseInt(username)} :     // use qid
+                {username: username}            // use username
+        }).then(async player => {
+            if (!matches(password, player.pwDigested))
+                return Promise.reject()
+            const payload = {
+                id: player.id,
+                isSiteAdmin: player.isSiteAdmin,
+                authorizedServers: await prisma.playerInServer.findMany({
+                    where: {
+                        playerId: player.id,
+                        isOperator: true
+                    }
+                }).then(result => result.map(entry => entry.serverId))
+            }
+            res.json({...payload, jwt: jwt.sign(payload, jwtSecret, {expiresIn: "12h"})})
+        }).catch(_error => res.status(400).end())
+    }
+)
--- src/http/register/submit.ts
+++ src/http/register/submit.ts
@@ -1,87 +1,86 @@
-import {validator} from "@exodus/schemasafe";
 import {Express} from "express";
 import {PrismaClient} from "@prisma/client";
 import {digest} from "../../utils/digest";
 import {qidExists} from "./check-qid";
 import {usernameExists} from "./check-username";
 import {randomUUID} from "node:crypto";
-
-const registerSubmitValidator = validator({
-    type: "object",
-    properties: {
-        qid: {type: "integer", minimum: 10001, maximum: 4294967295},
-        username: {type: "string", pattern: "^[a-zA-Z][a-zA-Z0-9_]{2,15}$"},
-        password: {type: "string", pattern: "^(?=.*[a-zA-Z])(?=.*\\d).{8,}$"},
-        invitationCode: {type: "string"},
-        "cf-turnstile-response": {type: "string"}
-    },
-    required: ["qid", "username", "password", "cf-turnstile-response"],
-    additionalProperties: false
-})
+import {jsonValidate} from "../../utils/json-schema-middleware";
 
 const validityPeriod = 10 * 60 * 1000 // ten minutes
-
 const emailAddr = "rc@gdt.pub"
 
-module.exports = (app: Express, prisma: PrismaClient) => app.post("/register/submit", async (req, res) => {
-    if (!registerSubmitValidator(req.body)) {
-        res.status(400).json({
+module.exports = (app: Express, prisma: PrismaClient) => app.post(
+    "/register/submit",
+    jsonValidate(
+        {
+            type: "object",
+            properties: {
+                qid: {type: "integer", minimum: 10001, maximum: 4294967295},
+                username: {type: "string", pattern: "^[a-zA-Z][a-zA-Z0-9_]{2,15}$"},
+                password: {type: "string", pattern: "^(?=.*[a-zA-Z])(?=.*\\d).{8,}$"},
+                invitationCode: {type: "string"},
+                "cf-turnstile-response": {type: "string"}
+            },
+            required: ["qid", "username", "password", "cf-turnstile-response"],
+            additionalProperties: false
+        },
+        res => res.status(400).json({
             reason: "info-invalid"
-        }).end()
-        return
-    }
-
-    const {qid, username, password, invitationCode, ["cf-turnstile-response"]: cfTurnstileResponse} = req.body
-
-    if (!await require("../../utils/captcha-verify")(cfTurnstileResponse)) {
-        res.status(400).json({
-            reason: "bad-captcha-response"
         })
-        return
-    }
+    ),
+    async (req, res) => {
+        const {qid, username, password, invitationCode, ["cf-turnstile-response"]: cfTurnstileResponse} = req.body
 
-    if (await qidExists(qid, prisma)) {
-        res.status(400).json({
-            reason: "qid-exists"
-        }).end()
-        return
-    }
-
-    if (await usernameExists(username, prisma)) {
-        res.status(400).json({
-            reason: "username-exists"
-        }).end()
-        return
-    }
-
-    if (invitationCode != null) {
-        if (await prisma.invitationCode.findUnique({
-            where: {
-                value: invitationCode
-            }
-        }) == null) {
+        if (!await require("../../utils/captcha-verify")(cfTurnstileResponse)) {
             res.status(400).json({
-                reason: "wrong-invitation-code"
+                reason: "bad-captcha-response"
             })
             return
         }
-    }
 
-    const passkey = randomUUID()
-
-    prisma.preRegisteredPlayer.create({
-        data: {
-            passkey: passkey,
-            qid: qid,
-            username: username,
-            pwDigested: digest(password),
-            expiresAt: Date.now() + validityPeriod,
-            invitationCode: invitationCode as string
+        if (await qidExists(qid, prisma)) {
+            res.status(400).json({
+                reason: "qid-exists"
+            }).end()
+            return
         }
-    }).then(_player => {
-        res.json({
-            emailAddr: emailAddr,
-            passkey: passkey
+
+        if (await usernameExists(username, prisma)) {
+            res.status(400).json({
+                reason: "username-exists"
+            }).end()
+            return
+        }
+
+        if (invitationCode != null) {
+            if (await prisma.invitationCode.findUnique({
+                where: {
+                    value: invitationCode
+                }
+            }) == null) {
+                res.status(400).json({
+                    reason: "wrong-invitation-code"
+                })
+                return
+            }
+        }
+
+        const passkey = randomUUID()
+
+        prisma.preRegisteredPlayer.create({
+            data: {
+                passkey: passkey,
+                qid: qid,
+                username: username,
+                pwDigested: digest(password),
+                expiresAt: Date.now() + validityPeriod,
+                invitationCode: invitationCode as string
+            }
+        }).then(_player => {
+            res.json({
+                emailAddr: emailAddr,
+                passkey: passkey
+            })
         })
-    })
-})
+    }
+)
