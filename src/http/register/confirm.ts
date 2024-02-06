94ac11d4959d50f7368a700590beec63ac6ee952
be <beoppchaser@gmail.com>
2024-02-06 23:58:46 +0800

    implement register

---
+++ src/http/register/confirm.ts
@@ -0,0 +1,18 @@
+// cf worker回调函数
+import { Express } from "express";
+import { unverifiedPasskeysCallback } from "../../ws/register/submit";
+
+module.exports = (app: Express) => app.get("/register/confirm", async (req, res) => {
+    const passkey = req.query.passkey;
+    if (!passkey) {
+        res.status(400).end();
+        return;
+    }
+    const callback = unverifiedPasskeysCallback.get(passkey);
+    if (!callback) {
+        res.status(400).end();
+        return;
+    }
+    callback();
+    res.status(200).end();
+});
