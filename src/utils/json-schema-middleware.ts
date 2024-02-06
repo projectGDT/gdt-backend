e76740eb0e531aeb029c408c03e8b273adc43550
Wesley F. Young <willamwei@outlook.com>
2024-02-07 01:18:27 +0800

    middleware instead of embedded validation

--- src/utils/json-schema-middleware.ts
+++ src/utils/json-schema-middleware.ts
@@ -1,15 +1,27 @@
 import {Schema, validator} from "@exodus/schemasafe";
-import {Request as JWTRequest} from "express-jwt";
-import {NextFunction, Response} from "express";
+import {Request, NextFunction, Response} from "express";
+import * as WebSocket from "ws"
 
 export function jsonValidate(
     schema: Schema,
     errorAction = (res: Response) => res.status(400).end()
-): (req: JWTRequest, res: Response, next: NextFunction) => void {
+): (req: Request, res: Response, next: NextFunction) => void {
     const jsonValidator = validator(schema)
     return (req, res, next) => {
         if (!jsonValidator(req.body))
             errorAction(res)
+        else next()
+    }
+}
+
+export function jsonValidateWs(
+    schema: Schema,
+    errorAction = (ws: WebSocket) => ws.close(400)
+): (ws: WebSocket, req: Request, next: NextFunction) => void {
+    const jsonValidator = validator(schema)
+    return (ws, req, next) => {
+        if (!jsonValidator(req.body))
+            errorAction(ws)
         else next()
     }
 }
