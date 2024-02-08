4e15f1794754eed447befa238dcd8d5f6854ac9e
Wesley F. Young <willamwei@outlook.com>
2024-02-09 02:29:57 +0800

    complete xbox binding

--- src/http/post-login/profile/bind/xbox.ts
+++ src/http/post-login/profile/bind/xbox.ts
@@ -1,33 +1,47 @@
 import {Express} from "express";
 import {PrismaClient} from "@prisma/client";
 import {Request} from "express-jwt";
-import {xbl} from "@xboxreplay/xboxlive-auth";
+import {live, xbl} from "@xboxreplay/xboxlive-auth";
 import {jsonValidate} from "../../../../utils/json-schema-middleware";
+import {string} from "random-js";
+
+const clientId = "9e474b67-edcd-4d23-b2fc-6dc8db5e08f7" // projectGDT
+const scope = "XboxLive.signin"
+const redirectUri = "http://localhost:3000/settings/profile/xbox"
 
 module.exports = (app: Express, prisma: PrismaClient) => app.post(
-    "/post-login/profile/bind/java-microsoft",
+    "/post-login/profile/bind/xbox",
     jsonValidate({
         type: "object",
         properties: {
-            userToken: {type: "string"}
+            code: {type: "string"}
         },
-        required: ["userToken"]
+        required: ["code"]
     }),
     (req: Request, res) => {
-        const {userToken} = req.body
-        xbl.exchangeTokenForXSTSToken(userToken).then(response => ({
+        const {code} = req.body
+        live.exchangeCodeForAccessToken(code, clientId, scope, redirectUri)
+        .then(response => xbl.exchangeRpsTicketForUserToken(response.access_token, "d"))
+        .then(response => xbl.exchangeTokenForXSTSToken(response.Token))
+        .then(response => ({
             xuid: response.DisplayClaims.xui[0].xid,
             xboxGamerTag: response.DisplayClaims.xui[0].gtg
-        })).then(({xuid, xboxGamerTag}) => prisma.profile.create({
+        })).catch(_err => {
+            throw "internal-error"
+        }).then(({xuid, xboxGamerTag}) => prisma.profile.create({
             data: {
                 uniqueIdProvider: -3, // Xbox
                 uniqueId: xuid,
                 playerId: req.auth.id,
                 cachedPlayerName: xboxGamerTag
             }
-        })).then(({uniqueId, cachedPlayerName}) => res.json({
+        })).catch(err => {
+            throw err instanceof string ? err : "already-exists"
+        }).then(({uniqueId, cachedPlayerName}) => res.json({
             xuid: uniqueId,
             xboxGamerTag: cachedPlayerName
-        })).catch(_err => res.status(502).end())
+        })).catch(errMsg => res.status(500).json({
+            reason: errMsg
+        }))
     }
 )
