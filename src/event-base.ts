809669a53eb5d2e1100c1d274d42972253b15cde
Wesley F. Young <willamwei@outlook.com>
2024-02-19 01:09:25 +0800

    now fire() automatically assign timestamp to the event

--- src/event-base.ts
+++ src/event-base.ts
@@ -2,6 +2,7 @@ import {EventEmitter} from "node:events";
 
 export abstract class GDTEvent {
     static typeId: string
+    timestamp: number = 0
 }
 
 export class GDTEventEmitter extends EventEmitter {
@@ -10,11 +11,11 @@ export class GDTEventEmitter extends EventEmitter {
     }
 
     // an alternative of on(eventName, listener)
-    // usage: listen<EventType>(EventType, (event, timestamp) => ...)
+    // usage: listen<EventType>(EventType, event => ...)
     // the compiler will infer the type of "event" param
     // There's no way to determine whether T and "type" is the same.
     // So it all depends on the caller.
-    listen<T extends GDTEvent>(type: typeof GDTEvent, listener: (event: T, timestamp: number) => void) {
+    listen<T extends GDTEvent>(type: typeof GDTEvent, listener: (event: T) => void) {
         return super.on(type.typeId, listener)
     }
 
@@ -22,7 +23,8 @@ export class GDTEventEmitter extends EventEmitter {
     // usage: fire(EventType, event)
     // the compiler will infer the type of "event" param.
     fire(type: typeof GDTEvent, event: GDTEvent) {
-        return super.emit(type.typeId, event, Date.now())
+        event.timestamp = Date.now()
+        return super.emit(type.typeId, event)
     }
 }
 
